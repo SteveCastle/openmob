417c5574d96b08c0dedeab5232ade7934530c626
Steve Castle <steve@tigerhaunt.com>
2019-01-07 18:32:25 -0800

    build primary many to many table

--- packages/shrike/db.sh
+++ packages/shrike/db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Docker command to run an empty docker db with dev tern settings.
 docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=tern -e POSTGRES_USER=tern -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data  mdillon/postgis
+docker ps
--- packages/shrike/migrations/1_initial.sql
+++ packages/shrike/migrations/1_initial.sql
@@ -1,12 +1,19 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
+CREATE FUNCTION count_not_nulls(variadic p_array anyarray)
+RETURNS BIGINT AS
+$$
+    SELECT count(x) FROM unnest($1) AS x
+$$ LANGUAGE SQL IMMUTABLE;
+
 -- CORE TABLES
 CREATE TABLE cause
 (
     id serial PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    title VARCHAR(255) NOT NULL
 );
 CREATE TABLE account
 (
@@ -64,6 +71,12 @@ CREATE TABLE election
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP
 );
+CREATE TABLE issue
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
 CREATE TABLE candidate
 (
     id serial PRIMARY KEY,
@@ -78,128 +91,198 @@ CREATE TABLE district
     geom geometry(Polygon,
     28992)
 );
-    CREATE TABLE office
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE live_event
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE live_event_type
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
+CREATE TABLE district_type
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE office
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE petition
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE volunteer_opportunity
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE volunteer_opportunity_type
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE live_event
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE live_event_type
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
 
-    CREATE TABLE boycott
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE company
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    --COMMERCE TABLES
-    CREATE TABLE product
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE product_type
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE donation
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE customer_cart
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE customer_order
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE payment
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE delivery
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    -- CRM FIELDS
-    CREATE TABLE contact
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE voter
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE agent
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE territory
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE activity
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE activity_type
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
-    CREATE TABLE note
-    (
-        id serial PRIMARY KEY,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP
-    );
+CREATE TABLE boycott
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE company
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+--COMMERCE TABLES
+CREATE TABLE product
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE product_type
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE donation
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE customer_cart
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE customer_order
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE payment
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE delivery
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+-- CRM FIELDS
+CREATE TABLE contact
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE petition_signer
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE voter
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE volunteer
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE agent
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE territory
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE activity
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE activity_type
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+CREATE TABLE note
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE cause_relationship
+(
+    id serial PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    cause_id INTEGER REFERENCES cause(id) NOT NULL,
+    account_id INTEGER REFERENCES account(id),
+    landing_page_id INTEGER REFERENCES landing_page(id),
+    boycott_id INTEGER REFERENCES boycott(id),
+    election_id INTEGER REFERENCES election(id),
+    petition_id INTEGER REFERENCES petition(id),
+    volunteer_opportunity_id INTEGER REFERENCES volunteer_opportunity(id),
+    live_event_id INTEGER REFERENCES live_event(id),
+    product_id INTEGER REFERENCES product(id),
+    donation_id INTEGER REFERENCES donation(id),
+    contact_id INTEGER REFERENCES contact(id),
+    agent_id INTEGER REFERENCES agent(id),
+
+    CONSTRAINT has_an_attachable
+CHECK(count_not_nulls(account_id,
+        landing_page_id,
+        boycott_id,
+        election_id,
+        petition_id,
+        volunteer_opportunity_id,
+        live_event_id,
+        product_id,
+        donation_id,
+        contact_id,
+        agent_id) = 1
+    )
+);
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-
+DROP FUNCTION count_not_nulls;
 -- CORE TABLES
+DROP TABLE cause_relationship;
 DROP TABLE cause;
 DROP TABLE account;
 DROP TABLE acl;
@@ -221,9 +304,14 @@ DROP TABLE delivery;
 DROP TABLE landing_page;
 DROP TABLE experiment;
 DROP TABLE election;
+DROP TABLE issue;
 DROP TABLE candidate;
 DROP TABLE district;
+DROP TABLE district_type;
 DROP TABLE office;
+DROP TABLE petition;
+DROP TABLE volunteer_opportunity;
+DROP TABLE volunteer_opportunity_type;
 DROP TABLE live_event;
 DROP TABLE live_event_type;
 DROP TABLE boycott;
@@ -232,6 +320,8 @@ DROP TABLE company;
 -- CRM TABLES
 DROP TABLE contact;
 DROP TABLE voter;
+DROP TABLE petition_signer;
+DROP TABLE volunteer;
 DROP TABLE agent;
 DROP TABLE territory;
 DROP TABLE activity;
