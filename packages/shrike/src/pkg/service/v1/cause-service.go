26a67e5462147764a124c1a7c951c5288a4fa61b
Steve Castle <steve@tigerhaunt.com>
2019-01-19 12:24:39 -0800

    getting service setup

--- packages/shrike/src/pkg/service/v1/cause-service.go
+++ packages/shrike/src/pkg/service/v1/cause-service.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"time"
 
-	"github.com/golang/protobuf/ptypes"
+	v1 "github.com/SteveCastle/openmob/packages/shrike/src/pkg/api/v1"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"../../api/v1/cause"
 )
 
 const (
@@ -17,30 +16,18 @@ const (
 	apiVersion = "v1"
 )
 
-// toDoServiceServer is implementation of v1.ToDoServiceServer proto interface
-type toDoServiceServer struct {
+// causeServiceServer is implementation of v1.CauseServiceServer proto interface
+type causeServiceServer struct {
 	db *sql.DB
 }
 
-// NewToDoServiceServer creates ToDo service
-func NewToDoServiceServer(db *sql.DB) v1.ToDoServiceServer {
-	return &toDoServiceServer{db: db}
-}
-
-// checkAPI checks if the API version requested by client is supported by server
-func (s *toDoServiceServer) checkAPI(api string) error {
-	// API version is "" means use current version of the service
-	if len(api) > 0 {
-		if apiVersion != api {
-			return status.Errorf(codes.Unimplemented,
-				"unsupported API version: service implements API version '%s', but asked for '%s'", apiVersion, api)
-		}
-	}
-	return nil
+// NewCauseServiceServer creates Cause service
+func NewCauseServiceServer(db *sql.DB) v1.CauseServiceServer {
+	return &causeServiceServer{db: db}
 }
 
 // connect returns SQL database connection from the pool
-func (s *toDoServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
+func (s *causeServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
 	c, err := s.db.Conn(ctx)
 	if err != nil {
 		return nil, status.Error(codes.Unknown, "failed to connect to database-> "+err.Error())
@@ -49,11 +36,7 @@ func (s *toDoServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
 }
 
 // Create new todo task
-func (s *toDoServiceServer) Create(ctx context.Context, req *v1.CreateRequest) (*v1.CreateResponse, error) {
-	// check if the API version requested by client is supported by server
-	if err := s.checkAPI(req.Api); err != nil {
-		return nil, err
-	}
+func (s *causeServiceServer) CreateCause(ctx context.Context, req *v1.CreateCauseRequest) (*v1.CreateCauseResponse, error) {
 
 	// get SQL connection from pool
 	c, err := s.connect(ctx)
@@ -62,36 +45,26 @@ func (s *toDoServiceServer) Create(ctx context.Context, req *v1.CreateRequest) (
 	}
 	defer c.Close()
 
-	reminder, err := ptypes.Timestamp(req.ToDo.Reminder)
+	// insert Cause entity data
+	res, err := c.ExecContext(ctx, "INSERT INTO Cause(`Title`) VALUES(?)",
+		req.Item.Title)
 	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "reminder field has invalid format-> "+err.Error())
+		return nil, status.Error(codes.Unknown, "failed to insert into Cause-> "+err.Error())
 	}
 
-	// insert ToDo entity data
-	res, err := c.ExecContext(ctx, "INSERT INTO ToDo(`Title`, `Description`, `Reminder`) VALUES(?, ?, ?)",
-		req.ToDo.Title, req.ToDo.Description, reminder)
-	if err != nil {
-		return nil, status.Error(codes.Unknown, "failed to insert into ToDo-> "+err.Error())
-	}
-
-	// get ID of creates ToDo
+	// get ID of creates Cause
 	id, err := res.LastInsertId()
 	if err != nil {
-		return nil, status.Error(codes.Unknown, "failed to retrieve id for created ToDo-> "+err.Error())
+		return nil, status.Error(codes.Unknown, "failed to retrieve id for created Cause-> "+err.Error())
 	}
 
-	return &v1.CreateResponse{
-		Api: apiVersion,
-		Id:  id,
+	return &v1.CreateCauseResponse{
+		Id: id,
 	}, nil
 }
 
 // Read todo task
-func (s *toDoServiceServer) Read(ctx context.Context, req *v1.ReadRequest) (*v1.ReadResponse, error) {
-	// check if the API version requested by client is supported by server
-	if err := s.checkAPI(req.Api); err != nil {
-		return nil, err
-	}
+func (s *causeServiceServer) GetCause(ctx context.Context, req *v1.GetCauseRequest) (*v1.GetCauseResponse, error) {
 
 	// get SQL connection from pool
 	c, err := s.connect(ctx)
@@ -100,41 +73,35 @@ func (s *toDoServiceServer) Read(ctx context.Context, req *v1.ReadRequest) (*v1.
 	}
 	defer c.Close()
 
-	// query ToDo by ID
-	rows, err := c.QueryContext(ctx, "SELECT `ID`, `Title`, `Description`, `Reminder` FROM ToDo WHERE `ID`=?",
+	// query Cause by ID
+	rows, err := c.QueryContext(ctx, "SELECT `id`, `title` FROM cause WHERE `id`=?",
 		req.Id)
 	if err != nil {
-		return nil, status.Error(codes.Unknown, "failed to select from ToDo-> "+err.Error())
+		return nil, status.Error(codes.Unknown, "failed to select from Cause-> "+err.Error())
 	}
 	defer rows.Close()
 
 	if !rows.Next() {
 		if err := rows.Err(); err != nil {
-			return nil, status.Error(codes.Unknown, "failed to retrieve data from ToDo-> "+err.Error())
+			return nil, status.Error(codes.Unknown, "failed to retrieve data from Cause-> "+err.Error())
 		}
-		return nil, status.Error(codes.NotFound, fmt.Sprintf("ToDo with ID='%d' is not found",
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Cause with ID='%d' is not found",
 			req.Id))
 	}
 
-	// get ToDo data
-	var td v1.ToDo
-	var reminder time.Time
-	if err := rows.Scan(&td.Id, &td.Title, &td.Description, &reminder); err != nil {
-		return nil, status.Error(codes.Unknown, "failed to retrieve field values from ToDo row-> "+err.Error())
-	}
-	td.Reminder, err = ptypes.TimestampProto(reminder)
-	if err != nil {
-		return nil, status.Error(codes.Unknown, "reminder field has invalid format-> "+err.Error())
+	// get Cause data
+	var td v1.Cause
+	if err := rows.Scan(&td.Id, &td.Title); err != nil {
+		return nil, status.Error(codes.Unknown, "failed to retrieve field values from Cause row-> "+err.Error())
 	}
 
 	if rows.Next() {
-		return nil, status.Error(codes.Unknown, fmt.Sprintf("found multiple ToDo rows with ID='%d'",
+		return nil, status.Error(codes.Unknown, fmt.Sprintf("found multiple Cause rows with ID='%d'",
 			req.Id))
 	}
 
-	return &v1.ReadResponse{
-		Api:  apiVersion,
-		ToDo: &td,
+	return &v1.GetCauseResponse{
+		Item: &td,
 	}, nil
 
 }
