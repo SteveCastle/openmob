ecd15ead9806ad1612294423a74c264fc6d8a2fc
Steve Castle <steve@tigerhaunt.com>
2019-01-20 19:14:20 -0800

    add grpc gateway

--- packages/shrike/go.mod
+++ packages/shrike/go.mod
@@ -2,9 +2,12 @@ module github.com/SteveCastle/openmob/packages/shrike
 
 require (
 	github.com/cridenour/go-postgis v1.0.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/golang/protobuf v1.2.0
+	github.com/grpc-ecosystem/grpc-gateway v1.6.4
 	github.com/lib/pq v1.0.0
 	golang.org/x/net v0.0.0-20190110200230-915654e7eabc
 	google.golang.org/genproto v0.0.0-20190111180523-db91494dd46c
 	google.golang.org/grpc v1.18.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- packages/shrike/src/pkg/cmd/cmd.go
+++ packages/shrike/src/pkg/cmd/cmd.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/lib/pq"
 
 	"github.com/SteveCastle/openmob/packages/shrike/src/pkg/protocol/grpc"
+	"github.com/SteveCastle/openmob/packages/shrike/src/pkg/protocol/rest"
 	v1 "github.com/SteveCastle/openmob/packages/shrike/src/pkg/service/v1"
 )
 
@@ -18,7 +19,9 @@ type Config struct {
 	// gRPC server start parameters section
 	// gRPC is TCP port to listen by gRPC server
 	GRPCPort string
-
+	// HTTP/REST gateway start parameters section
+	// HTTPPort is TCP port to listen by HTTP/REST gateway
+	HTTPPort string
 	// DB Datastore parameters section
 	// DatastoreDBHost is host of database
 	DatastoreDBHost string
@@ -37,6 +40,7 @@ func RunServer() error {
 	// get configuration
 	var cfg Config
 	flag.StringVar(&cfg.GRPCPort, "grpc-port", "9090", "gRPC port to bind")
+	flag.StringVar(&cfg.HTTPPort, "http-port", "8080", "HTTP port to bind")
 	flag.StringVar(&cfg.DatastoreDBHost, "db-host", "localhost", "Database host")
 	flag.StringVar(&cfg.DatastoreDBUser, "db-user", "tern", "Database user")
 	flag.StringVar(&cfg.DatastoreDBPassword, "db-password", "tern", "Database password")
@@ -45,6 +49,10 @@ func RunServer() error {
 
 	if len(cfg.GRPCPort) == 0 {
 		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
+	}
+
+	if len(cfg.HTTPPort) == 0 {
+		return fmt.Errorf("invalid TCP port for HTTP gateway: '%s'", cfg.HTTPPort)
 	}
 	// Datbase connection string.
 	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
@@ -59,6 +67,9 @@ func RunServer() error {
 	defer db.Close()
 
 	v1API := v1.NewCauseServiceServer(db)
-
+	// run HTTP gateway
+	go func() {
+		_ = rest.RunServer(ctx, cfg.GRPCPort, cfg.HTTPPort)
+	}()
 	return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
 }
