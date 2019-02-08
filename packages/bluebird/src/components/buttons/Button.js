12ac9721273e866a5d81fdd56e8a70a93571988a
Steve Castle <steve@tigerhaunt.com>
2019-02-08 08:28:07 -0800

    setup storybook and libraries

---
+++ packages/bluebird/src/components/buttons/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: #000000;
+  border: none;
+  border-radius: 4px;
+  color: #a5aed5;
+  font-size: 1rem;
+  padding: 16px;
+`;
+
+export default function Button({ children }) {
+  return <StyledButton>{children}</StyledButton>;
+}
