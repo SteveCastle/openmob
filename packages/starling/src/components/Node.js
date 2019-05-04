6a85a70820c0e76d365e81515249f8e25723de7c
Steve Castle <steve@tigerhaunt.com>
2019-05-04 13:25:16 -0700

    map module type fields to props in layout nodes

--- packages/starling/src/components/Node.js
+++ packages/starling/src/components/Node.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Loadable from 'react-loadable'
-import { useStaticQuery, graphql } from 'gatsby'
 import ErrorBoundary from './ErrorBoundary'
 import Spinner from '@openmob/bluebird/src/components/loaders/Spinner'
 const getFieldValue = field => {
@@ -8,7 +7,7 @@ const getFieldValue = field => {
     case 'string':
       return field.StringValue
     case 'graphql':
-      return field.DataPath
+      return JSON.parse(field.DataPathValue)
     default:
       return field.StringValue
   }
@@ -28,27 +27,10 @@ const getProps = fields =>
   )
 
 function Node({ path, fields, id }) {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        wren {
-          getCause(ID: "c75db1d7-17ac-47b1-aac2-879a6761b4cc") {
-            ID
-            ElectionMemberships {
-              Election {
-                ID
-                Title
-              }
-            }
-          }
-        }
-      }
-    `
-  )
   const Component = getComponent(path)
   return (
     <ErrorBoundary>
-      <Component {...getProps(fields)} key={id} data={data} />
+      <Component {...getProps(fields)} key={id} />
     </ErrorBoundary>
   )
 }
--- packages/starling/src/templates/page.js
+++ packages/starling/src/templates/page.js
@@ -80,6 +80,7 @@ export const pageQuery = graphql`
                     DateTimeValue {
                       seconds
                     }
+                    DataPathValue
                     DataPath
                   }
                   ComponentImplementation {
