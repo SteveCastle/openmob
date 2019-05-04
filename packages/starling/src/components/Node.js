7c1b294f7e41594f4a4f049bdde7c9bfca94c5ba
Steve Castle <steve@tigerhaunt.com>
2019-05-04 13:35:05 -0700

    rename module field data type

--- packages/starling/src/components/Node.js
+++ packages/starling/src/components/Node.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Loadable from 'react-loadable'
 import ErrorBoundary from './ErrorBoundary'
 import Spinner from '@openmob/bluebird/src/components/loaders/Spinner'
+
+// Return the field data to map to props given the field types data type.
 const getFieldValue = field => {
   switch (field.FieldType.DataType) {
     case 'string':
       return field.StringValue
-    case 'graphql':
+    case 'module':
       return JSON.parse(field.DataPathValue)
     default:
       return field.StringValue
