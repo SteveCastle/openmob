c6fd2e5cb11d240f349746a6333986396bbc9458
Steve Castle <steve@tigerhaunt.com>
2019-07-07 13:41:50 -0700

    fix missing component weight

--- packages/starling/src/queries/getPage.js
+++ packages/starling/src/queries/getPage.js
@@ -18,6 +18,7 @@ const GET_PAGE = gql`
               Weight
               Components {
                 ID
+                Weight
                 Fields {
                   ID
                   FieldType {
